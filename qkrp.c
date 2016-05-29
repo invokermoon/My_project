e4568669a8e1f9bc29e2a1a20fbcf7de6bd8033a
Huix Fang <huix.fang@intel.com>
2016-05-30 01:01:08 +0800

    step 1----this is more stable , and the reset is only the tree

--- qkrp.c
+++ qkrp.c
@@ -68,9 +68,21 @@ int this_cpu;
 uint32_t qattrs;
 uint32_t memleaks;
 
-static struct qdev_s{
+/*Below parameters is about memleak*/
+/* minimum and maximum address that may be valid pointers */
+static unsigned long min_addr = ULONG_MAX;
+static unsigned long max_addr;
+/* the list of all allocated objects */
+static LIST_HEAD(object_list);
+/* the list of gray-colored objects (see color_gray comment below) */
+static LIST_HEAD(gray_list);
+/* search tree for object boundaries */
+static struct rb_root object_tree_root = RB_ROOT;
+/* rw_lock protecting the access to object_list and object_tree_root */
+static DEFINE_RWLOCK(kmemleak_lock);
+
+static struct qkrp_dev_s{
 	struct kprobe qkrp;
-	struct kprobe qkmp;
 	int id;
 	unsigned long cnt;
 	unsigned long avr_duration;
@@ -81,25 +93,42 @@ static struct qdev_s{
 	unsigned long pre_nsec;
 	unsigned long post_nsec;
 	char name[10];
-} qdev[MAX_NUM];
+} qkrp_dev[MAX_NUM];
+
+struct qkmp_object{
+	spinlock_t lock;
+	unsigned long flags;		/* object status flags */
+	struct list_head object_list;
+	struct list_head gray_list;
+	struct rb_node rb_node;
+	struct rcu_head rcu;		/* object_list lockless traversal */
+	/* object usage count; object freed when use_count == 0 */
+	atomic_t use_count;
+	unsigned long pointer;
+	size_t size;
+	/* minimum number of a pointers found before it is considered leak */
+	int min_count;
+	/* the total number of pointers found pointing to this object */
+	int count;
+	/* checksum for detecting modified objects */
+	u32 checksum;
+	/* memory ranges to be scanned inside an object (empty for all) */
+	struct hlist_head area_list;
+	unsigned long trace[MAX_TRACE];
+	unsigned int trace_len;
+	unsigned long jiffies;		/* creation timestamp */
+	pid_t pid;			/* pid of the current task */
+	char comm[TASK_COMM_LEN];	/* executable name */
+};
+
 
 #if 0
-/*Below parameters is about memleak*/
-/* the list of all allocated objects */
-static LIST_HEAD(object_list);
-/* the list of gray-colored objects (see color_gray comment below) */
-static LIST_HEAD(gray_list);
-/* search tree for object boundaries */
-static struct rb_root object_tree_root = RB_ROOT;
-/* rw_lock protecting the access to object_list and object_tree_root */
-static DEFINE_RWLOCK(kmemleak_lock);
-
 /*
  * Scan a memory block (exclusive range) for valid pointers and add those
  * found to the gray list.
  */
 static void scan_block(void *_start, void *_end,
-		       struct kmemleak_object *scanned)
+		       struct qkmp_object *scanned)
 {
 	unsigned long *ptr;
 	unsigned long *start = PTR_ALIGN(_start, BYTES_PER_POINTER);
@@ -108,7 +137,7 @@ static void scan_block(void *_start, void *_end,
 
 	read_lock_irqsave(&kmemleak_lock, flags);
 	for (ptr = start; ptr < end; ptr++) {
-		struct kmemleak_object *object;
+		struct qkmp_object *object;
 		unsigned long pointer;
 
 		if (scan_should_stop())
@@ -184,52 +213,171 @@ static void scan_large_block(void *start, void *end)
 }
 #endif
 
-static int do_pre_vfree(struct kprobe *p, struct pt_regs *regs)
+static struct qkmp_object *create_object(unsigned long ptr, size_t size,
+					     int min_count, gfp_t gfp)
 {
-	printk(KERN_INFO"[pre]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	unsigned long flags;
+	struct qkmp_object *object, *parent;
+	struct rb_node **link, *rb_parent;
+
+	object = kmem_cache_alloc(object_cache, gfp_kmemleak_mask(gfp));
+	if (!object) {
+		pr_warning("Cannot allocate a qkmp_object structure\n");
+		kmemleak_disable();
+		return NULL;
+	}
+
+	INIT_LIST_HEAD(&object->object_list);
+	INIT_LIST_HEAD(&object->gray_list);
+	INIT_HLIST_HEAD(&object->area_list);
+	spin_lock_init(&object->lock);
+	atomic_set(&object->use_count, 1);
+	object->flags = OBJECT_ALLOCATED;
+	object->pointer = ptr;
+	object->size = size;
+	object->min_count = min_count;
+	object->count = 0;			/* white color initially */
+	object->jiffies = jiffies;
+	object->checksum = 0;
+
+	/* task information */
+	if (in_irq()) {
+		object->pid = 0;
+		strncpy(object->comm, "hardirq", sizeof(object->comm));
+	} else if (in_softirq()) {
+		object->pid = 0;
+		strncpy(object->comm, "softirq", sizeof(object->comm));
+	} else {
+		object->pid = current->pid;
+		/*
+		 * There is a small chance of a race with set_task_comm(),
+		 * however using get_task_comm() here may cause locking
+		 * dependency issues with current->alloc_lock. In the worst
+		 * case, the command line is not correct.
+		 */
+		strncpy(object->comm, current->comm, sizeof(object->comm));
+	}
+
+	/* kernel backtrace */
+	object->trace_len = __save_stack_trace(object->trace);
+
+	write_lock_irqsave(&kmemleak_lock, flags);
+
+	min_addr = min(min_addr, ptr);
+	max_addr = max(max_addr, ptr + size);
+	link = &object_tree_root.rb_node;
+	rb_parent = NULL;
+	while (*link) {
+		rb_parent = *link;
+		parent = rb_entry(rb_parent, struct qkmp_object, rb_node);
+		if (ptr + size <= parent->pointer)
+			link = &parent->rb_node.rb_left;
+		else if (parent->pointer + parent->size <= ptr)
+			link = &parent->rb_node.rb_right;
+		else {
+			kmemleak_stop("Cannot insert 0x%lx into the object "
+				      "search tree (overlaps existing)\n",
+				      ptr);
+			/*
+			 * No need for parent->lock here since "parent" cannot
+			 * be freed while the kmemleak_lock is held.
+			 */
+			dump_object_info(parent);
+			kmem_cache_free(object_cache, object);
+			object = NULL;
+			goto out;
+		}
+	}
+	rb_link_node(&object->rb_node, rb_parent, link);
+	rb_insert_color(&object->rb_node, &object_tree_root);
+
+	list_add_tail_rcu(&object->object_list, &object_list);
+out:
+	write_unlock_irqrestore(&kmemleak_lock, flags);
+	return object;
+}
+
+
+
+static int test_pre(struct kprobe *p, struct pt_regs *regs)
+{
+	int i = 0;
+	printk(KERN_INFO"[testpre]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	printk(KERN_INFO"[testpre]:reg->ax=0x%lx\n",regs->ax);
+	printk(KERN_INFO"[testpre]:reg->di=0x%lx\n",regs->di);
+	printk(KERN_INFO"[testpre]:reg->dx=0x%lx\n",regs->dx);
 	return 0;
 }
 
-static void do_post_vfree(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
+static void test_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
 {
-	printk(KERN_INFO"[post]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	int i = 0;
+	printk(KERN_INFO"[testpost]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	printk(KERN_INFO"[testpost]:reg->ax=0x%lx\n",regs->ax);
+	printk(KERN_INFO"[testpost]:reg->di=0x%lx\n",regs->di);
+	printk(KERN_INFO"[testpost]:reg->dx=0x%lx\n",regs->dx);
+
 }
-static int do_fault_vfree(struct kprobe *p, struct pt_regs *regs, int trapnr)
+static int test_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
 {
-	printk(KERN_INFO"do vfree fault done\n");
+	printk(KERN_INFO"do fault done\n");
 	return 0;
 }
 
-static int do_pre_vmalloc(struct kprobe *p, struct pt_regs *regs)
+struct kprobe test_qkmp = {
+	.pre_handler = test_pre,
+	.post_handler= test_post,
+	.fault_handler= test_fault,
+};
+
+static int do_ret_vfree(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
-	printk(KERN_INFO"[pre]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	//printk(KERN_INFO"[post]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	return 0;
+}
+static int do_entry_vmalloc(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	/*parameter of vmalloc is DI */
+	printk(KERN_INFO"[entry]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	size = (uint32_t *)(regs->di);
+	if (size){
+		return 0;
+	}
+	return 1;
+}
+
+static int do_ret_vmalloc(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	/*ret of vmalloc is ax */
+	//printk(KERN_INFO"[ret]:pid:%d|comm:%10s,reg->di=%lu\n",current->pid, current->comm, regs->di);
+	printk(KERN_INFO"[ret]:reg->ax=0x%lx\n",regs->ax);
+	printk(KERN_INFO"[ret]:reg->di=0x%lx\n",regs->di);
+	printk(KERN_INFO"[ret]:reg->dx=0x%lx\n",regs->dx);
+	unsigned long long *ptr= regs->ax;
+	if ( ptr && !IS_ERR(ptr)){
+		create_object((unsigned long)ptr, size, min_count);
+	}
 	return 0;
 }
 
-static void do_post_vmalloc(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
-{
-	printk(KERN_INFO"[post]:pid:%d|comm:%10s,reg->di=%lu,addr=0x%x\n",current->pid, current->comm, regs->di,p->addr);
-	printk(KERN_INFO"[post]:reg->bp=0x%x\n",regs->bp);
-	printk(KERN_INFO"[post]:reg->bx=0x%x\n",regs->bx);
-	printk(KERN_INFO"[post]:reg->si=0x%x\n",regs->si);
-	printk(KERN_INFO"[post]:reg->ax=0x%x\n",regs->ax);
-	printk(KERN_INFO"[post]:reg->dx=0x%x\n",regs->ax);
-}
-static int do_fault_vmalloc(struct kprobe *p, struct pt_regs *regs, int trapnr)
-{
-	printk(KERN_INFO"do vmalloc fault done\n");
-	return 0;
-}
+struct kretprobe vmalloc_qkmp = {
+	.handler = do_ret_vmalloc,
+	.entry_handler = do_entry_vmalloc,
+};
+
+struct kretprobe free_qkmp={
+	.handler = do_ret_vfree,
+};
 
 static int do_pre_handler(struct kprobe *p, struct pt_regs *regs)
 {
 	int i = 0;
 	//printk(KERN_INFO"[pre]:pid:%d|comm:%10s,reg->di=0x%x\n",current->pid, current->comm, regs->di);
 	for(i=0;i<index;i++){
-		if(p->addr == qdev[i].qkrp.addr){
+		if(p->addr == qkrp_dev[i].qkrp.addr){
 				this_cpu = raw_smp_processor_id();
-				qdev[i].pre_t = cpu_clock(this_cpu);
-				qdev[i].pre_nsec= do_div(qdev[i].pre_t, 1000000000);
+				qkrp_dev[i].pre_t = cpu_clock(this_cpu);
+				qkrp_dev[i].pre_nsec= do_div(qkrp_dev[i].pre_t, 1000000000);
 		}
 	}
 	return 0;
@@ -240,19 +388,19 @@ static void do_post_handler(struct kprobe *p, struct pt_regs *regs, unsigned lon
 	int i = 0;
 	//printk(KERN_INFO"[post]:pid:%d|comm:%10s,reg->di=0x%x\n",current->pid, current->comm, regs->di);
 	for(i=0;i<index;i++){
-		if(p->addr == qdev[i].qkrp.addr){
-			qdev[i].cnt++;
+		if(p->addr == qkrp_dev[i].qkrp.addr){
+			qkrp_dev[i].cnt++;
 			//this_cpu = raw_smp_processor_id();
-			qdev[i].post_t = cpu_clock(this_cpu);
-			qdev[i].post_nsec= do_div(qdev[i].post_t, 1000000000);
-			qdev[i].cur_duration = qdev[i].post_nsec-qdev[i].pre_nsec;
-			qdev[i].total_duration=qdev[i].total_duration+qdev[i].cur_duration;
-			qdev[i].avr_duration=(qdev[i].total_duration)/qdev[i].cnt;
+			qkrp_dev[i].post_t = cpu_clock(this_cpu);
+			qkrp_dev[i].post_nsec= do_div(qkrp_dev[i].post_t, 1000000000);
+			qkrp_dev[i].cur_duration = qkrp_dev[i].post_nsec-qkrp_dev[i].pre_nsec;
+			qkrp_dev[i].total_duration=qkrp_dev[i].total_duration+qkrp_dev[i].cur_duration;
+			qkrp_dev[i].avr_duration=(qkrp_dev[i].total_duration)/qkrp_dev[i].cnt;
 #if 0
 			printk(KERN_INFO"pre[%5lu,%7lu],post[%5lu,%7lu],cur_duration=%lu,avr_duration=%lu\n",
-												(unsigned long)qdev[i].pre_t,qdev[i].pre_nsec,
-												(unsigned long)qdev[i].post_t,qdev[i].post_nsec,
-												qdev[i].cur_duration,qdev[i].avr_duration);
+												(unsigned long)qkrp_dev[i].pre_t,qkrp_dev[i].pre_nsec,
+												(unsigned long)qkrp_dev[i].post_t,qkrp_dev[i].post_nsec,
+												qkrp_dev[i].cur_duration,qkrp_dev[i].avr_duration);
 #endif
 		}
 	}
@@ -311,9 +459,9 @@ static ssize_t show_qattrs(struct device *dev,
 	if(!index)
 		return scnprintf(buf, PAGE_SIZE, "Please input right func,when you insmod\n");
 	for(i=0; i<index;i++){
-		ret +=scnprintf(buf+ret, PAGE_SIZE-ret, "%s:%lu<ns>:%lu<cnt>\n",qdev[i].name,
-																	qdev[i].avr_duration,
-																	qdev[i].cnt
+		ret +=scnprintf(buf+ret, PAGE_SIZE-ret, "%s:%lu<ns>:%lu<cnt>\n",qkrp_dev[i].name,
+																	qkrp_dev[i].avr_duration,
+																	qkrp_dev[i].cnt
 																	);
 	}
 	return ret;
@@ -336,6 +484,7 @@ static const struct file_operations qkrp_device_fops = {
 	.owner		= THIS_MODULE,
 };
 
+
 static int __init qkrp_init(void)
 {
 	int ret;
@@ -347,26 +496,26 @@ static int __init qkrp_init(void)
 	while(funcs_name && !strncmp("sys", funcs_name, 3)){
 		func_name = strsep(&funcs_name, ",");
 		if(!strncmp("sys", func_name, 3)){
-			qdev[index].qkrp.addr = (kprobe_opcode_t *)kallsyms_lookup_name(func_name);
-			if(qdev[index].qkrp.addr == NULL) goto err_exit;
-			//printk(KERN_INFO "func_name=%s,addr=0x%x\n",func_name,qdev[index].qkrp.addr);
-			qdev[index].qkrp.pre_handler = do_pre_handler;
-			qdev[index].qkrp.post_handler= do_post_handler;
-			qdev[index].qkrp.fault_handler= do_fault_handler;
+			qkrp_dev[index].qkrp.addr = (kprobe_opcode_t *)kallsyms_lookup_name(func_name);
+			if(qkrp_dev[index].qkrp.addr == NULL) goto err_exit;
+			//printk(KERN_INFO "func_name=%s,addr=0x%x\n",func_name,qkrp_dev[index].qkrp.addr);
+			qkrp_dev[index].qkrp.pre_handler = do_pre_handler;
+			qkrp_dev[index].qkrp.post_handler= do_post_handler;
+			qkrp_dev[index].qkrp.fault_handler= do_fault_handler;
 
-			qdev[index].id = index;
-			qdev[index].cnt = 0;
-			qdev[index].total_duration = 0;
-			qdev[index].cur_duration = 0;
-			qdev[index].avr_duration= 0;
-			strncpy(qdev[index].name, func_name, strlen(func_name));
-			printk(KERN_ERR"name=%s\n",qdev[index].name);
+			qkrp_dev[index].id = index;
+			qkrp_dev[index].cnt = 0;
+			qkrp_dev[index].total_duration = 0;
+			qkrp_dev[index].cur_duration = 0;
+			qkrp_dev[index].avr_duration= 0;
+			strncpy(qkrp_dev[index].name, func_name, strlen(func_name));
+			printk(KERN_ERR"name=%s\n",qkrp_dev[index].name);
 
 		}else{
 			printk(KERN_ERR"For stable, we just profile the sys_XXX\n");
 			goto err_exit;
 		}
-		ret = register_kprobe(&(qdev[index].qkrp));
+		ret = register_kprobe(&(qkrp_dev[index].qkrp));
 		if( ret < 0 ){
 			printk(KERN_ERR"register kprobe qkrp %d error\n",index);
 			goto err_exit;
@@ -374,35 +523,41 @@ static int __init qkrp_init(void)
 		index++;
 	}
 /*register the kmp*/
-	qdev[0].qkmp.addr = (kprobe_opcode_t *)kallsyms_lookup_name("vmalloc");
-	if(qdev[0].qkmp.addr == NULL) {
+#ifdef TEST
+	test_qkmp.addr = (kprobe_opcode_t *)kallsyms_lookup_name("vmalloc");
+	if(test_qkmp.addr == NULL) {
 		printk(KERN_ERR"Vmalloc kprobe failed\n");
 		goto err_exit;
 	}
-	qdev[0].qkmp.pre_handler = do_pre_vmalloc;
-	qdev[0].qkmp.post_handler= do_post_vmalloc;
-	qdev[0].qkmp.fault_handler= do_fault_vmalloc;
-
-	qdev[1].qkmp.addr = (kprobe_opcode_t *)kallsyms_lookup_name("vfree");
-	if(qdev[1].qkmp.addr == NULL) {
+	ret = register_kprobe(&(test_qkmp));
+	if( ret < 0 ){
+		printk(KERN_ERR"register kprobe test error\n");
+		goto err_exit;
+	}
+#else
+	vmalloc_qkmp.kp.addr = (kprobe_opcode_t *)kallsyms_lookup_name("vmalloc");
+	if(vmalloc_qkmp.kp.addr == NULL) {
+		printk(KERN_ERR"Vmalloc kprobe failed\n");
+		goto err_exit;
+	}
+	free_qkmp.kp.addr = (kprobe_opcode_t *)kallsyms_lookup_name("vfree");
+	if(free_qkmp.kp.addr == NULL) {
 		printk(KERN_ERR"Vfree kprobe failed\n");
 		goto err_exit;
 	}
-	qdev[1].qkmp.pre_handler = do_pre_vfree;
-	qdev[1].qkmp.post_handler= do_post_vfree;
-	qdev[1].qkmp.fault_handler= do_fault_vfree;
 
-	ret = register_kprobe(&(qdev[0].qkmp));
+	ret = register_kretprobe(&(vmalloc_qkmp));
 	if( ret < 0 ){
 		printk(KERN_ERR"register kprobe malloc error\n");
 		goto err_exit;
 	}
 
-	ret = register_kprobe(&(qdev[1].qkmp));
+	ret = register_kretprobe(&(free_qkmp));
 	if( ret < 0 ){
 		printk(KERN_ERR"register kprobe vfree error\n");
 		goto err_exit;
 	}
+#endif
 
 	kfree(bak_p);
 
@@ -455,7 +610,7 @@ err_exit_chrdev:
 	unregister_chrdev(major, "qkrp");
 err_exit_kprob:
 	for(i=0;i<index;i++){
-		unregister_kprobe(&(qdev[i].qkrp));
+		unregister_kprobe(&(qkrp_dev[i].qkrp));
 	}
 err_exit:
 	printk(KERN_INFO"register kprobe qkrp error\n");
@@ -465,11 +620,17 @@ err_exit:
 static void __exit qkrp_exit(void)
 {
 	int i = 0;
+#ifdef TEST
+	unregister_kretprobe(&(test_qkmp));
+#else
+	unregister_kretprobe(&(vmalloc_qkmp));
+	unregister_kretprobe(&(free_qkmp));
+#endif
 	device_destroy(qkrp_class, MKDEV(major, 0));
 	class_unregister(qkrp_class);
 	unregister_chrdev(major, "qkrp");
 	for(i=0;i<index;i++){
-		unregister_kprobe(&(qdev[i].qkrp));
+		unregister_kprobe(&(qkrp_dev[i].qkrp));
 	}
 	printk(KERN_INFO"unregister kprobe qkrp done\n");
 }
